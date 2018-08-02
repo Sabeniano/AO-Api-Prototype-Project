822684f02d732a53fa0715fe7e68e49552c9f5a9
BalenD <bade@pc-tec.local>
2018-08-02 08:23:38 +0200

    fixed duplicate error in validator, changed employee validation to check if value exists first

--- server/middleware/validationMiddleware/validateEmployeeFields.js
+++ server/middleware/validationMiddleware/validateEmployeeFields.js
@@ -1,14 +1,14 @@
 const { body } = require('express-validator/check');
 
 exports.createFields = [
-  body('firstName', 'must specify a first name').isString().exists(),
-  body('lastName', 'must specify a last name').isString().exists(),
-  body('birthday', 'must specify a valid date').isISO8601().isBefore(Date.now().toString()),
-  body('email', 'must specify a valid email').isString().isEmail(),
-  body('city', 'must specify a city').isString().exists(),
-  body('country', 'must specify a country').isString().exists(),
-  body('street', 'must specify a street').isString().exists(),
-  body('phoneNumber', 'must specify a mobile phone number').isNumeric().isMobilePhone(),
+  body('firstName', 'must specify a valid first name').exists().isString(),
+  body('lastName', 'must specify a valid last name').exists().isString(),
+  body('birthday', 'must specify a valid date').exists().isISO8601().isBefore(Date.now().toString()),
+  body('email', 'must specify a valid email').exists().isString().isEmail(),
+  body('city', 'must specify a valid city').exists().isString(),
+  body('country', 'must specify a country').exists().isString().exists(),
+  body('street', 'must specify a street').exists().isString(),
+  body('phoneNumber', 'must specify a mobile phone number').exists().isNumeric().isMobilePhone(),
   body('startDate', 'must specify a validate date').isISO8601(),
   body('lastChanged', 'must be left empty').isEmpty(),
 ];
