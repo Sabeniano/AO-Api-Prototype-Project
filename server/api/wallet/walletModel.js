0e87d8e8a675b295ebec5196532bbf2426d38051
Mikkel MFH. Frydenlund Hammer <miha@pc-tec.local>
2018-08-02 08:06:51 +0200

    Added new timeformat to wallet lastChanged

--- server/api/wallet/walletModel.js
+++ server/api/wallet/walletModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const moment = require('moment-timezone');
 const hlGenerator = require('../../utils/hyperMediaLinkGenerator'); 
+
+const currentTime = new Date(moment().add(2, 'hours').format());
 
 const walletSchema = new mongoose.Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, default: new mongoose.Types.ObjectId() },
@@ -7,7 +10,7 @@ const walletSchema = new mongoose.Schema({
   salary: { type: Number, default: 0 },
   paymentMethod: { type: String, enum: ['Monthly', 'Hourly'], default: 'Hourly' },
   employee_id: { type: String, required: true },
-  lastChanged: { type: Date, default: Date.now },
+  lastChanged: { type: Date, default: currentTime },
   links: {
     type: [{
       _id: false,
