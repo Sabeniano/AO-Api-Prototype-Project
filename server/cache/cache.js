448a7721cb86c5da1bd1e316f6f26f4bf3cddca6
sabeniano <jason_tolentino@hotmail.com>
2018-09-21 09:49:56 +0200

    Added comments, removed JSON.stringify, still need to add function to different routes

--- server/cache/cache.js
+++ server/cache/cache.js
@@ -2,16 +2,17 @@ const redis = require('redis');
 
 const client = redis.createClient('6379', '127.0.0.1', { no_ready_check: true });
 
-
+// Checks to see if the key is in the DB, takes the data in the DB if it is
+// If not it will save the data from the URL, and save it for X amount of time
 const redisMiddleware = (req, res, next) => {
-  const key = "__redisKEY__" + req.originalUrl || req.url;
+  const key = '__redisKEY__' + req.originalUrl || req.url;
   client.get(key, (err, reply) => {
     if (reply) {
       res.send(reply);
     } else {
       res.sendResponse = res.send;
       res.send = (body) => {
-        client.set(key, JSON.stringify(body), 'EX', 120); // The last parameter is to choose how many seconds you want to have the key in the DB
+        client.set(key, body, 'EX', 120); // The last parameter is to choose how many seconds you want to have the key in the DB
         res.sendResponse(body);
       };
       next();
