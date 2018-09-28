5abb898293fb59bb9eed1437aa5d7db6c2619a5c
sabeniano <jason_tolentino@hotmail.com>
2018-09-28 08:57:10 +0200

    made it possible to add different ttl(time to live) on each route

--- server/api/employee/employeeRouter.js
+++ server/api/employee/employeeRouter.js
@@ -4,14 +4,14 @@ const MessageService = require('../../utils/messageService');
 const verifyRole = require('../../middleware/authMiddleware/verifyRole');
 const validationErrorHandler = require('../../middleware/validationMiddleware/validationErrorHandler');
 const validateEmployeeFields = require('../../middleware/validationMiddleware/validateEmployeeFields');
-const redisMiddleware = require('../../middleware/cacheMiddleware/cache.js');
+const redisMiddleware = require('../../middleware/cacheMiddleware/cache');
 
 // Using the specifik methods it sends the data from employeeController to api.js
 // Only the methods with attached parameters will work
 employeeRouter.param('id', employeeController.params);
 
 employeeRouter.route('/')
-  .get(redisMiddleware, employeeController.GetAllEmployees)
+  .get(redisMiddleware(2), employeeController.GetAllEmployees)
   .post(verifyRole(), validateEmployeeFields.createFields, validationErrorHandler(), employeeController.CreateEmployee)
   .patch(MessageService(405, 'Use /employee/ID to update specific resource'))
   .delete(MessageService(405, 'Use /employee/ID to delete specific resource'));
--- server/middleware/cacheMiddleware/cache.js
+++ server/middleware/cacheMiddleware/cache.js
@@ -4,20 +4,23 @@ const client = redis.createClient('6379', '127.0.0.1', { no_ready_check: true })
 
 // Checks to see if the key is in the DB, takes the data in the DB if it is
 // If not it will save the data from the URL, and save it for X amount of time
-const redisMiddleware = (req, res, next) => {
-  const key = `__redisKEY__${req.originalUrl}` || req.url;
-  client.get(key, (reply) => {
-    if (reply) {
-      res.send(reply);
-    } else {
-      res.sendResponse = res.send;
-      res.send = (body) => {
-        client.set(key, body, 'EX', 120); // The last parameter is to choose how many seconds you want to have the key in the DB
-        res.sendResponse(body);
-      };
-      next();
-    }
-  });
-};
+
+function redisMiddleware(duration) {
+  return (req, res, next) => {
+    const key = `__KEY__${req.originalUrl}` || req.url;
+    client.get(key, (err, reply) => {
+      if (reply) {
+        res.send(reply);
+      } else {
+        res.sendResponse = res.send;
+        res.send = (body) => {
+          client.set(key, body, 'EX', duration * 60); // The last parameter is to choose how many seconds you want to have the key in the DB
+          res.sendResponse(body);
+        };
+        next();
+      }
+    });
+  };
+}
 
 module.exports = redisMiddleware;
